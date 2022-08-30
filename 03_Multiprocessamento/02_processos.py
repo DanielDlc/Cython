0c6e537e6f5ea0657cb1b70ac84843c5228e7881
DanielDlc <dlc.engcomputacao@gmail.com>
2022-08-30 07:39:52 -0300

    multiprocessamento

---
+++ 03_Multiprocessamento/02_processos.py
@@ -0,0 +1,22 @@
+import multiprocessing
+
+print(
+    f'iniciando o processo com nome: {multiprocessing.current_process().name}')
+
+
+def faz_algo(valor):
+    print(f'Fazedo algo com o {valor}')
+
+
+def main():
+    pc = multiprocessing.Process(target=faz_algo, args=(
+        'Pássaro',), name='Processo Daniel Dlc')
+
+    print(f'Iniciando o processo com o nome: {pc.name}')
+
+    pc.start()
+    pc.join()
+
+
+if __name__ == '__main__':
+    main()
