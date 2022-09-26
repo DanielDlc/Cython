06ac368ad00fcdd4ea4f8b35ff9b1be49863efd9
DanielDlc <dlc.engcomputacao@gmail.com>
2022-09-26 19:39:04 -0300

    Asyncio Io Files

---
+++ 04_Programação_Assíncrona/08_asyncio_IoFiles.py
@@ -0,0 +1,25 @@
+import asyncio
+
+import aiofiles
+
+
+async def exemplo_arquivo_01():
+    async with aiofiles.open('10_texto.txt') as arquivo:
+        conteudo = await arquivo.read()
+    print(conteudo)
+
+
+async def exemplo_arquivo_02():
+    async with aiofiles.open('09_links.txt') as arquivo:
+        async for linha in arquivo:
+            print(linha)
+
+
+def main():
+    el = asyncio.get_event_loop()
+    el.run_until_complete(exemplo_arquivo_02())
+    el.close()
+
+
+if __name__ == '__main__':
+    main()
