4d16d44bd419af2b6ae38d959851a398a67da253
DanielDlc <dlc.engcomputacao@gmail.com>
2023-12-20 14:04:57 +0000

    FastAPI hello

---
+++ 06_FastAPI/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get('/')
+async def index():
+    return {"Hello": "FastAPI"}
+
+
+@app.get('/produtos/{id}')
+async def buscar_produto(id: int):
+    return {"id": id}
